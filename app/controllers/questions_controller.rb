032c430392f1e57ba90bde0feba724fdc94bf928
Yoko Kasai <ykookaasi@gmail.com>
2020-11-09 20:43:39 +0100

    today's work

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    if @question.downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
